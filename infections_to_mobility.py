a46c22ea7fcd414e163f829aac516a85610ecbd4
nathanielwang01 <nathanielwang01@gmail.com>
2020-09-20 00:17:11 -0400

    cahnges to infections_to_mobility.py again

--- infections_to_mobility.py
+++ infections_to_mobility.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
 # Import CSV data from file
 all_data = pd.read_csv('data/all_data.csv')
@@ -12,20 +13,33 @@ all_data = pd.read_csv('data/all_data.csv')
 # If point is cumulative +/- tolerance, mark as trend & set new anchor @ point
 # If point is final point, mark as trend and end
 
-TOLERANCE = 50
+#TOLERANCE = 100
 
 change_by_date = all_data[['date', 'grocery_and_pharmacy_percent_change_from_baseline']]
 
+model = LinearRegression()
+x = np.array([i for i in range(210)]).reshape(-1,1)
+model.fit(x, change_by_date['grocery_and_pharmacy_percent_change_from_baseline'])
+y = model.predict(x)
+
+plt.scatter(change_by_date['date'], change_by_date['grocery_and_pharmacy_percent_change_from_baseline'])
+plt.plot(x, y)
+plt.show()
+
+'''
 #anchor = change_by_date[0]['grocery_and_pharmacy_percent_change_from_baseline']
 current = 0
 anchor_idx = [0]
 length = change_by_date.shape[0]
 
+plt.scatter(change_by_date['date'], change_by_date['grocery_and_pharmacy_percent_change_from_baseline'])
+plt.show()
+
 for i in range(1, length):
     if change_by_date['grocery_and_pharmacy_percent_change_from_baseline'][i] != 'nan':
         current += change_by_date['grocery_and_pharmacy_percent_change_from_baseline'][i]
 
-    print(current)
+    #print(change_by_date['grocery_and_pharmacy_percent_change_from_baseline'][i])
     if current >= TOLERANCE or current <= -TOLERANCE:
         anchor_idx.append(i)
         current = 0
@@ -37,3 +51,4 @@ if anchor_idx[-1] != length - 1:
     anchor_idx.append(length - 1)
 
 print(anchor_idx)
+'''
