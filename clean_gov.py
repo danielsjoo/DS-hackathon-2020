94be682bfce33bacaf7711053e3d6508b651e2a6
Andrew Tang <andrewtang0926@gmail.com>
2020-09-19 20:30:14 -0700

    a

--- clean_gov.py
+++ clean_gov.py
@@ -6,11 +6,11 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('data/Covid_govt_data.csv')
 us_data = df[df.country_name == 'United States']
 uk_data = df[df.country_name == 'United Kingdom']
-jp_data = df[df.country_name == 'Japan']
+jp_data = df[df.country_name == 'Japan'
 
 #start date check
 us_min = us_data['date'].min()
 uk_min = uk_data['date'].min()
 jp_min = jp_data['date'].min()
-start_date = max(us_min, uk_min, jp_min)
+start_date = max(us_min, uk_min, jp_min) #2020-01-01
 print(start_date)
