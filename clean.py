87a71904690b2b4ed35ba2a5d4faa1e728f90426
Andrew Tang <andrewtang0926@gmail.com>
2020-09-19 19:32:38 -0700

    asdf

---
+++ clean.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#retrieve data 
+policy_data = pd.read_csv('data/Covid_govt_data.csv')
+us_data = policy_data[policy_data.country_name == 'United States']
+uk_data = policy_data[policy_data.country_name == 'United Kingdom']
+cn_data = policy_data[policy_data.country_name == 'China']
+
+#standardize 
+def standardize(dataframe df):
+    
