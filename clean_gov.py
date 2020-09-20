cc530c2ae6581e31ff221b596b98a4a9e18b5bb1
Andrew Tang <andrewtang0926@gmail.com>
2020-09-19 21:31:47 -0700

    a

--- clean_gov.py
+++ clean_gov.py
@@ -4,18 +4,28 @@ import matplotlib.pyplot as plt
 
 #retrieve data
 df = pd.read_csv('data/Covid_govt_data.csv')
-us_data = df[df.country_name == 'United States']
-uk_data = df[df.country_name == 'United Kingdom']
-jp_data = df[df.country_name == 'Japan']
+us = df[df.country_name == 'United States']
+uk = df[df.country_name == 'United Kingdom']
+jp = df[df.country_name == 'Japan']
+
+#remove workplace_closing
+us = us.drop("workplace_closing", axis=1)
+uk = uk.drop("workplace_closing", axis=1)
+jp = jp.drop("workplace_closing", axis=1)
+
+#remove row if NaN
+us = us.dropna(axis=0)
+uk = uk.dropna(axis=0)
+jp = jp.dropna(axis=0)
 
 #to csv
-us_data.to_csv('data/us_gov.csv')
-uk_data.to_csv('data/uk_gov.csv')
-jp_data.to_csv('data/jp_gov.csv')
+us.to_csv('data/us_gov.csv')
+uk.to_csv('data/uk_gov.csv')
+jp.to_csv('data/jp_gov.csv')
 
 #start date check
-us_min = us_data['date'].min()
-uk_min = uk_data['date'].min()
-jp_min = jp_data['date'].min()
+us_min = us['date'].min()
+uk_min = uk['date'].min()
+jp_min = jp['date'].min()
 start_date = max(us_min, uk_min, jp_min) #2020-01-01
 print(start_date)
