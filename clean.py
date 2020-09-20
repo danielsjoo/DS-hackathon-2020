3a5bf210ca3f2e9f3aa57b6e935c87461c22a068
Andrew Tang <andrewtang0926@gmail.com>
2020-09-19 19:59:10 -0700

    a

--- clean.py
+++ clean.py
@@ -7,7 +7,3 @@ policy_data = pd.read_csv('data/Covid_govt_data.csv')
 us_data = policy_data[policy_data.country_name == 'United States']
 uk_data = policy_data[policy_data.country_name == 'United Kingdom']
 cn_data = policy_data[policy_data.country_name == 'China']
-
-#standardize 
-def standardize(dataframe df):
-    
