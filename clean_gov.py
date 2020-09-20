bf1c93f5b074c468b331fbaf3177cc4054afe6d7
Andrew Tang <andrewtang0926@gmail.com>
2020-09-19 20:29:11 -0700

    a

--- clean_gov.py
+++ clean_gov.py
@@ -7,7 +7,6 @@ df = pd.read_csv('data/Covid_govt_data.csv')
 us_data = df[df.country_name == 'United States']
 uk_data = df[df.country_name == 'United Kingdom']
 jp_data = df[df.country_name == 'Japan']
-print(us_data)
 
 #start date check
 us_min = us_data['date'].min()
