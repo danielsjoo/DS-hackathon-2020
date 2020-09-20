d79fb1800dae0312c7d70bfd9a7031837c3fe906
nathanielwang01 <nathanielwang01@gmail.com>
2020-09-20 00:21:12 -0400

    clean_gov.py set back to actually deal with govt_data

--- clean_gov.py
+++ clean_gov.py
@@ -3,10 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #retrieve data
-df = pd.read_csv('data/all_data.csv')
-us_data = df[df.country_region == 'United States']
-uk_data = df[df.country_region == 'United Kingdom']
-jp_data = df[df.country_region == 'Japan']
+df = pd.read_csv('data/gov_data.csv')
+us_data = df[df.country_name == 'United States']
+uk_data = df[df.country_name == 'United Kingdom']
+jp_data = df[df.country_name == 'Japan']
 print(us_data)
 
 #start date check
